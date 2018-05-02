4a14b5858f639eb924bc3bec6dfaf7ff007cec97
iimuz <d.is.iimuz@gmail.com>
2018-05-02 09:47:51 +0000

    feat: Since peco does not work in wsl, fzf is introduced, and the part using peco is modified to be fzf
    * 変更は.bashrcに含まれるpecoを単純にfzfに置換しています。

--- setup-wsl_debian.sh
+++ setup-wsl_debian.sh
@@ -33,9 +33,24 @@ echo "export DOCKER_HOST='tcp://0.0.0.0:2375'" >> ~/.bashrc
 echo "\nif [ -f ~/.bashrc.local ]; then\n  . ~/.bashrc.local\nfi\n" >> ~/.bashrc
 wget --no-check-certificate https://raw.githubusercontent.com/iimuz/dotfiles/master/.bashrc -O ~/.bashrc.local
 
+# install git
 sudo apt install -y --no-install-recommends git
 wget --no-check-certificate https://raw.githubusercontent.com/iimuz/dotfiles/master/.gitconfig -O ~/.gitconfig
 
+# install ghq
+sudo apt install -y --no-install-recommends unzip
+wget https://github.com/motemen/ghq/releases/download/v0.8.0/ghq_linux_amd64.zip
+unzip ghq_linux_amd64.zip -d ghq
+sudo mv ghq/ghq /usr/bin/
+rm -rf ghq ghq_linux_amd64.zip .wget-hsts
+echo "[ghq]\n  root = ~/src\n" >> ~/.gitconfig.local
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+sudo sed -i 's/peco/fzf/' ~/.bashrc.local
+
+# install neovim
 sudo apt install -y --no-install-recommends neovim
 mkdir -p ~/.config/nvim
 wget --no-check-certificate https://raw.githubusercontent.com/iimuz/dotfiles/master/.vimrc -O ~/.config/nvim/init.vim
