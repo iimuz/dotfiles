a3ce12298b870bdef3ad241d384776f77deb57ea
iimuz <iz.primary.story+github@gmail.com>
2019-02-13 14:48:36 +0900

    fix: docker 関連のコマンドエイリアスを修正し、通常コマンドのように呼び出せるように修正

--- .config/docker/alias.sh
+++ .config/docker/alias.sh
@@ -1,31 +1,40 @@
+# docker を利用してコマンドを呼び出し、即時破棄する
+function docker_command {
+  image=$1
+  command=$2
+  docker run -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) $image $2 ${@:3}
+}
+
 # gcloud
 alias dgconfig='docker run -it --name gcloud-config -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) localhost:gcloud bash'
 alias dgcloud='docker run --rm -it --volumes-from gcloud-config -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) localhost:gcloud gcloud'
-alias dglist='dgcloud compute instances list'
-alias dgscp='dgcloud compute scp'
-alias dgssh='dgcloud compute ssh'
-alias dgstart='dgcloud compute instances start'
-alias dgstop='dgcloud compute instances stop'
+alias dgcdel='dgcloud compute instances delete'
+alias dgclist='dgcloud compute instances list'
+alias dgcscp='dgcloud compute scp'
+alias dgcssh='dgcloud compute ssh'
+alias dgcstart='dgcloud compute instances start'
+alias dgcstop='dgcloud compute instances stop'
 
 # git
-alias dgit='docker run --rm -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) iimuz/git:v1.1.0-gpg1'
+alias dgit='docker_command iimuz/git:v1.1.0-gpg1 git'
 alias dgitexec='docker exec -it gitenv su-exec git ash'
 alias dgitstart='docker start gitenv'
 
 # hugo
-alias dhugo='docker run --rm -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) iimuz/hugo:v0.47.1-1'
+alias dhugo='docker_command iimuz/hugo:v0.47.1-1 hugo'
+alias dhugos='docker_command iimuz/hugo:v0.47.1-1 ash'
 
 # neovim
-alias dnvim='docker run --rm -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) iimuz/neovim:v0.3.0-slim7'
-alias dnvim-cpp='docker run --rm -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) iimuz/neovim:v0.3.0-cpp7'
-alias dnvim-go='docker run --rm -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) iimuz/neovim:v0.3.0-golang1'
-alias dnvim-js='docker run --rm -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) iimuz/neovim:v0.3.0-node1'
-alias dnvim-md='docker run --rm -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) iimuz/neovim:v0.3.0-md7'
-alias dnvim-py='docker run --rm -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) iimuz/neovim:v0.3.0-py7'
+alias dnvim='docker_command iimuz/neovim:v0.3.0-slim7 nvim'
+alias dnvim-cpp='docker_command iimuz/neovim:v0.3.0-cpp7 nvim'
+alias dnvim-go='docker_command iimuz/neovim:v0.3.0-golang1 nvim'
+alias dnvim-js='docker_command iimuz/neovim:v0.3.0-node1 nvim'
+alias dnvim-md='docker_command iimuz/neovim:v0.3.0-md7 nvim'
+alias dnvim-py='docker_command iimuz/neovim:v0.3.0-py7 nvim'
 
 # python
-alias dpython='docker run --rm -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID=$(id -g) iimuz/python-dev:v3.7.0-pipenv1'
+alias dpython='docker_command iimuz/python-dev:v3.7.0-pipenv1 python'
 
 # travis
-alias dtravis='docker run --rm -it -v $(pwd):/src:rw -e USER_ID=$(id -u) -e GROUP_ID$(id -g) iimuz/travis-client:v1.8.9 gosu dev bash'
+alias dtravis='docker_command iimuz/travis-client:v1.8.9 travis'
 
