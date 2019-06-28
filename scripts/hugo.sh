2e7d2b6e5342ff0ae897ffbe018d6d83716035b8
iimuz <iz.primary.story+github@gmail.com>
2019-06-28 18:04:41 +0900

    fix: modify script to install hugo

--- scripts/hugo.sh
+++ scripts/hugo.sh
@@ -2,15 +2,16 @@
 #
 # Install hugo
 
-VERSION=0.49.2
+VERSION=0.55.5
 ARCH=64bit
 FILENAME=hugo_${VERSION}_Linux-${ARCH}.tar.gz
 TEMP_DIR=./hugo_expand
 
 # download hugo
 wget https://github.com/gohugoio/hugo/releases/download/v${VERSION}/${FILENAME}
+
 mkdir -p $TEMP_DIR
-tar xvzf hugo_0.49.2_Linux-64bit.tar.gz -C $TEMP_DIR
-sudo mv $TEMP_DIR/hugo /usr/local/bin/
+tar xvzf hugo_${VERSION}_Linux-64bit.tar.gz -C $TEMP_DIR
+mv $TEMP_DIR/hugo ~/.local/bin/
 rm -rf $TEMP_DIR $FILENAME
 
