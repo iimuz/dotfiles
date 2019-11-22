f3d3f75160756b756fdf5b6537e0c61d040c5dc5
iimuz <iz.primary.story+github@gmail.com>
2019-11-22 09:44:36 +0900

    fix: add install script for bitwarden cli.

---
+++ scripts/bitwarden.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Install bitwarden CLI.
+
+VERSION=1.8.0
+ARCH=linux
+USER_LOCAL=$HOME/.local/bin
+
+expand_dir=bw-${ARCH}-$VERSION
+archive_file=${expand_dir}.zip
+download_url=https://github.com/bitwarden/cli/releases/download/v${VERSION}/$archive_file
+
+# download bw
+wget $download_url
+
+# expand and replace
+mkdir -p $expand_dir
+unzip $archive_file -d $expand_dir
+chmod u+x $expand_dir/bw
+mv $expand_dir/bw $USER_LOCAL/
+rm -r $expand_dir $archive_file
