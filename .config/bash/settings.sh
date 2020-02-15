1eab9b6be73ffcdd8789481b9005fe705a3c5f81
iimuz <iz.primary.story+github@gmail.com>
2020-02-15 03:19:21 +0000

    fix: add guard for bash_history prompt command.

--- .config/bash/settings.sh
+++ .config/bash/settings.sh
@@ -1,4 +1,8 @@
+# set bash_history setting
 export HISTCONTROL=ignoreboth:erasedups
 shopt -u histappend
-export PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
+save_history="history -n; history -w; history -c; history -r"
+if [[ ";$PROMPT_COMMAND;" != *";$save_history;"* ]]; then
+  export PROMPT_COMMAND="$save_history; $PROMPT_COMMAND"
+fi
 
