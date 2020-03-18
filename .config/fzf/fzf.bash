b097bfa7fdf7ad53a08f34a421052f99570c895e
iimuz <iz.primary.story+github@gmail.com>
2020-03-18 22:00:28 +0900

    fix: add fzf command.

--- .config/fzf/fzf.bash
+++ .config/fzf/fzf.bash
@@ -21,3 +21,21 @@ if [ -f $FZF_KEYBINDINGS ]; then . $FZF_KEYBINDINGS; fi
 # Find repositories under golang directory structure
 alias ffghq='cd $(find ${SOURCE_ROOT:-$HOME/src} -follow  -maxdepth 3 -mindepth 3 -type d | fzf)'
 
+# Open the selected file with the default editor
+# Ref: https://github.com/junegunn/fzf/wiki/examples#opening-files
+fo() (
+  IFS=$'\n' out=("$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)")
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+  fi
+)
+
+# using ripgrep combined with preview
+# find-in-file - usage: fif <searchTerm>
+# Ref: https://github.com/junegunn/fzf/wiki/examples#searching-file-contents
+fif() {
+  if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+  rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
+}
