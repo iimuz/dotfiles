497bae642aa957485248e07bb102a0a6f6cf63fd
iimuz <iz.primary.story+github@gmail.com>
2020-03-28 21:03:13 +0900

    fix: gcloud command が読み込めなくなっていた問題を修正

--- .config/gcloud/gcloud.sh
+++ .config/gcloud/gcloud.sh
@@ -2,15 +2,15 @@
 #
 # Setting for gcloud command.
 
-# Gurad if command does not exist.
-if ! type gcloud > /dev/null 2>&1; then return 0; fi
-
 # Load gcloud setting file.
 path_bash="$HOME/google-cloud-sdk/path.bash.inc"
 if [ -f "$path_bash" ]; then source "$path_bash"; fi
 
 completion_bash="$HOME/google-cloud-sdk/completion.bash.inc"
 if [ -f "$completion_bash" ]; then source "$completion_bash"; fi
+
+# Gurad if command does not exist.
+if ! type gcloud > /dev/null 2>&1; then return 0; fi
 
 # Aliases.
 alias gcdel='gcloud compute instances delete'
@@ -20,4 +20,3 @@ alias gcscp='gcloud compute scp'
 alias gcssh='gcloud compute ssh'
 alias gcstart='gcloud compute instances start'
 alias gcstop='gcloud compute instances stop'
-
