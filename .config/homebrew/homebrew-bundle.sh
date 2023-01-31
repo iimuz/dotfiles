be6f3f3e6be234202736e54d16ff8d236feea0c4
iimuz <iz.primary.story+gihtub@gmail.com>
2023-01-31 19:47:35 +0900

    fix: homebrew bundle で mac(arm64), mac(rosetta), wslを使い分けるように設定を修正

--- .config/homebrew/homebrew-bundle.sh
+++ .config/homebrew/homebrew-bundle.sh
@@ -2,4 +2,14 @@
 # - [homebrew bundle](https://github.com/Homebrew/homebrew-bundle)
 
 # Brewfileの場所を設定
-export HOMEBREW_BUNDLE_FILE="$(cd $(dirname ${BASH_SOURCE:-0}); pwd)/Brewfile"
+if [ "$(uname)" = "Darwin" ]; then  # MacOS
+  if [ "$(uname -m)" = "arm64" ]; then  # Apple silicon
+    export HOMEBREW_BUNDLE_FILE="$(cd $(dirname ${(%):-%N}); pwd)/Brewfile-mac"
+  else  # Intel
+    export HOMEBREW_BUNDLE_FILE="$(cd $(dirname ${(%):-%N}); pwd)/Brewfile-rosetta"
+  fi
+elif [ "$(uname)" = "Linux" ]; then  # Linux
+  if [[ "$(uname -r)" == *microsoft* ]]; then  # WSL
+    export HOMEBREW_BUNDLE_FILE="$(cd $(dirname ${(%):-%N}); pwd)/Brewfile-wsl"
+  fi
+fi
