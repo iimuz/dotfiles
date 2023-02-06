4accfcd49f6b7c4e3429f203d4ba28f3c84f2db4
iimuz <iz.primary.story+gihtub@gmail.com>
2023-02-06 17:54:25 +0900

    fix: npm.shでスクリプトパスをzshだと取得できないので修正

--- .config/npm/npm.sh
+++ .config/npm/npm.sh
@@ -5,13 +5,16 @@
 # Gurad if command does not exist.
 if ! type npm > /dev/null 2>&1; then return 0; fi
 
+# npmrcを本スクリプトと同フォルダにあるnpmrcに設定
+# $1: 実行するスクリプトパス ex. ${BASH_SOURCE:-$0}
 function _change_default_npm_directory() {
-  local script_path="${BASH_SOURCE:-$0}"
+  local script_path="$1"
   if [ -L "$script_path" ]; then script_path=$(readlink -f "$script_path"); fi
-  local readonly SCRIPT_DIR=$(cd $(dirname "$script_path"); pwd)
+  local readonly script_path=$(cd $(dirname "$script_path"); pwd)
 
   # Change default npm directory.
-  export NPM_CONFIG_USERCONFIG=$SCRIPT_DIR/npmrc
+  export NPM_CONFIG_USERCONFIG=$script_path/npmrc
 }
 
-_change_default_npm_directory
+_change_default_npm_directory "${BASH_SOURCE:-$0}"
+
