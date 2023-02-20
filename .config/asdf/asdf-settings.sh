acdda47fcc386e92a207b3afa32ab9f298deedba
iimuz <iz.primary.story+github@gmail.com>
2023-02-20 11:41:50 +0900

    fix: asdfのarm64/rosetta切り替え時のパス削除を追加

--- .config/asdf/asdf-settings.sh
+++ .config/asdf/asdf-settings.sh
@@ -2,11 +2,20 @@
 #
 # asdfの設定ファイル。
 
+# === PATHの削除
+if [ -n "$ASDF_DIR" ]; then
+  remove_path "$ASDF_DIR/bin"
+fi
+if [ -n "$ASDF_DATA_DIR" ]; then
+  remove_path "$ASDF_DATA_DIR/shims"
+fi
+
 # === Gurad if command does not exist.
 # asdfの判定はhomebrewで導入しておりパスが設定されているか
 if [ ! -f "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh" ]; then return 0; fi
 
 # === data dir
+# ASDF_DIRについては、homebrewの場所が異なるので別のパスになるので、そのまま
 export ASDF_DATA_DIR="$HOME/.asdf"
 if [ "$(uname)" = "Darwin" ]; then  # MacOS
   if [ "$(uname -m)" = "arm64" ]; then  # Apple silicon
