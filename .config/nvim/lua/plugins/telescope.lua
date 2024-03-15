466379d53d3d8d41ad86644dd1c0747694ecc5ac
iimuz <iz.primary.story+github@gmail.com>
2024-03-15 15:30:22 +0900

    nvimにtelescopeのコマンドパレットを追加

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,27 @@
 -- Telescope.nvimの設定
-local opts = { noremap = true, silent = true }
 
--- `Ctr + P`でファイル一覧を表示
-vim.api.nvim_set_keymap("n", '<C-p>', '<cmd>Telescope find_files find_command=rg,--files,--hidden,--glob,!*.git<CR>', opts)
+-- `<Leader>p`でファイル一覧を表示
+vim.keymap.set(
+  "n",
+  '<Leader>p',
+  '<cmd>Telescope find_files find_command=rg,--files,--hidden,--glob,!*.git<CR>',
+  {desc = "⭐︎Find files."}
+)
+-- `<Leader>P`でコマンドパレットを表示
+vim.keymap.set(
+  "n",
+  '<Leader>P',
+  function()
+    require('telescope.builtin').keymaps()
+    vim.cmd("normal! i⭐︎")
+  end,
+  {desc = "⭐︎Open command palet."}
+)
+
+-- Telescopeのkeymapsで参照できるようにするため
+for k, v in pairs(require("telescope.builtin")) do
+  if type(v) == "function" then
+    vim.keymap.set('n', '<Plug>(telescope.' .. k .. ')', v)
+  end
+end
 
