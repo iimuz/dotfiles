489656199668ad5012b0e3f4de3209cd14a5b194
iimuz <iz.primary.story+github@gmail.com>
2024-03-17 23:03:07 +0900

    nvimで暫定の自作ショートカットを追加

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -5,3 +5,15 @@ vim.api.nvim_set_keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- 自作
+-- 現在開いているファイル名をクリップボードにコピーする
+vim.keymap.set(
+  "n",
+  "<Plug>(self.copy_filename)",
+  function()
+    local filename = vim.fn.expand("%:t:r")
+    vim.fn.setreg("+", filename)
+  end,
+  {desc = "⭐︎Self: Copy filename."}
+)
+
