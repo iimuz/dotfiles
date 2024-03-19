3a0d65ba1353f1bf6362f2b39fa82cf87956a1c2
iimuz <iz.primary.story+github@gmail.com>
2024-03-19 17:04:57 +0900

    nvimでmarkdownファイルのfront matterにあるlastmodを自動修正する暫定スクリプトを追加

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
-require("base")     -- プラグインなどに関係ない基本設定
-require("keymaps")  -- プラグインに関係ないキーマップの基本設定
-require("lsp")      -- Builtin LSP設定
-require("lazy-init")  -- plugin managerとしてlazy.nvimを設定
+require("base")         -- プラグインなどに関係ない基本設定
+require("keymaps")      -- プラグインに関係ないキーマップの基本設定
+require("lsp")          -- Builtin LSP設定
+require("lazy-init")    -- plugin managerとしてlazy.nvimを設定
 
+require("auto-lastmod") -- front matterのlatmod自動修正
