64a81d6828373eb46cd67cea340150ebdd29329b
iimuz <iz.primary.story+github@gmail.com>
2024-03-23 09:44:16 +0900

    change neovim color scheme to tokyonight-night

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -12,6 +12,7 @@ return {
   {
     "cocopon/iceberg.vim",
     cond = condition,
+    lazy = false,
     enabled = false,
     config = function()
       vim.opt.termguicolors = true
@@ -24,12 +25,35 @@ return {
   {
     "EdenEast/nightfox.nvim",
     cond = condition,
-    enabled = true,
+    lazy = false,
+    enabled = false,
     config = function()
       vim.opt.termguicolors = true
       vim.opt.background = "dark"
       vim.cmd [[ colorscheme nightfox ]]
     end,
-  }
+  },
+  {
+    'projekt0n/github-nvim-theme',
+    cond = condition,
+    lazy = false,
+    enabled = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup({})
+      vim.cmd [[ colorscheme github_dark ]]
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    cond = condition,
+    lazy = false,
+    enabled = true,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd [[ colorscheme tokyonight-night ]]
+    end,
+  },
 }
 
