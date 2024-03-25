848e52f9bd214d96570279347469c93484b6180a
iimuz <iz.primary.story+github@gmail.com>
2024-03-25 11:01:14 +0900

    add markdown previewer on neovim.

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,44 @@
+-- markdown-preview.nvim
+-- see: <https://github.com/iamcco/markdown-preview.nvim>
+--
+-- Preview markdown file.
+
+local set = vim.keymap.set
+set(
+  "n",
+  "<Plug>(MarkdownPreview.Load)",
+  "<cmd>MarkdownPreview<CR>",
+  { desc = "⭐︎MarkdownPreview: Load plugin." }
+)
+
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = {
+    "MarkdownPreviewToggle",
+    "MarkdownPreview",
+    "MarkdownPreviewStop",
+  },
+  ft = { "markdown" },
+  build = function()
+    vim.fn["mkdp#util#install"]()
+
+    set(
+      "n",
+      "<Plug>(MarkdownPreview.Open)",
+      "<cmd>MarkdownPreview<CR>",
+      { desc = "⭐︎MarkdownPreview: Show." }
+    )
+    set(
+      "n",
+      "<Plug>(MarkdownPreview.Stop)",
+      "<cmd>MarkdownPreviewStop<CR>",
+      { desc = "MarkdownPreview: Stop." }
+    )
+    set(
+      "n",
+      "<Plug>(MarkdownPreview.Toggle)",
+      "<cmd>MarkdownPreviewToggle<CR>",
+      { desc = "MarkdownPreview: Toggle." }
+    )
+  end,
+}
