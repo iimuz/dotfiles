5fad5510f8007e351e46e60c772e9a0488e125fe
iimuz <iz.primary.story+github@gmail.com>
2024-03-26 11:21:16 +0900

    neovimでformatとlastmodの更新をauto saveプラグインに依存するように修正

--- .config/nvim/lua/auto-lastmod.lua
+++ .config/nvim/lua/auto-lastmod.lua
@@ -1,51 +1,62 @@
 -- Markdownファイルのfront matterにあるlastmodを自動で保存日時に書き換える
 
 local function UpdateLastmod()
-  local lastmod = vim.fn.strftime("%Y-%m-%dT%H:%M:%S")              -- 現在の日時を取得
-  local buf = vim.api.nvim_get_current_buf()                        -- 現在のバッファを取得
-  local max_line = 10                                               -- 最大で読み込む行数
-  local lines = vim.api.nvim_buf_get_lines(buf, 0, max_line, false) -- 先頭からmax_line行のみ確認する
+	local lastmod = vim.fn.strftime("%Y-%m-%dT%H:%M:%S") -- 現在の日時を取得
+	local buf = vim.api.nvim_get_current_buf() -- 現在のバッファを取得
+	local max_line = 10 -- 最大で読み込む行数
+	local lines = vim.api.nvim_buf_get_lines(buf, 0, max_line, false) -- 先頭からmax_line行のみ確認する
 
-  -- frontmatterを見つける
-  -- ipairsは1から始まる
-  local start_line, end_line = nil, nil
-  for i, line in ipairs(lines) do
-    if start_line ~= nil and line:match("^---$") then
-      end_line = i
-      break
-    end
-
-    if start_line == nil and line:match("^---$") then
-      start_line = i
-    end
-  end
-  if end_line == nil then
-    end_line = 10
+  -- auto-saveから実行するときにファイルタイプを考慮しないので、ここで確認する
+  local filetype = vim.api.nvim_buf_get_option(buf, "filetype")
+  vim.notify("lastmod")
+  if filetype ~= "markdown" then
+    return
   end
 
-  if start_line and end_line then
-    -- lastmodフィールドを見つける
-    local lastmod_line = nil
-    for i = start_line, end_line do
-      if lines[i]:match("^lastmod:.*$") then
-        lastmod_line = i
-        break
-      end
-    end
+	-- frontmatterを見つける
+	-- ipairsは1から始まる
+	local start_line, end_line = nil, nil
+	for i, line in ipairs(lines) do
+		if start_line ~= nil and line:match("^---$") then
+			end_line = i
+			break
+		end
 
-    if lastmod_line then
-      local lastmod_str = "lastmod: " .. lastmod .. "+09:00"
-      print("Update lastmod: " .. lastmod_str)
-      vim.api.nvim_buf_set_lines(buf, lastmod_line - 1, lastmod_line, false, { lastmod_str })
-    end
-  end
+		if start_line == nil and line:match("^---$") then
+			start_line = i
+		end
+	end
+	if end_line == nil then
+		end_line = 10
+	end
+
+	if start_line and end_line then
+		-- lastmodフィールドを見つける
+		local lastmod_line = nil
+		for i = start_line, end_line do
+			if lines[i]:match("^lastmod:.*$") then
+				lastmod_line = i
+				break
+			end
+		end
+
+		if lastmod_line then
+			local lastmod_str = "lastmod: " .. lastmod .. "+09:00"
+			vim.notify("Update lastmod: " .. lastmod_str)
+			vim.api.nvim_buf_set_lines(buf, lastmod_line - 1, lastmod_line, false, { lastmod_str })
+		end
+	end
 end
 
 -- ファイル保存時にUpdateLastmod()を実行
-vim.api.nvim_create_autocmd(
-  { "BufWritePost" },
-  {
-    pattern = { "*.md" },
-    callback = UpdateLastmod,
-  }
-)
+local augroup = vim.api.nvim_create_augroup("auto-update-lastmod", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = augroup,
+	pattern = { "*.md" },
+	callback = UpdateLastmod,
+})
+vim.api.nvim_create_autocmd({ "User" }, {
+	group = augroup,
+	pattern = { "AutoSaveWritePre" },
+	callback = UpdateLastmod,
+})
--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -6,62 +6,67 @@
 -- - <https://github.com/josean-dev/dev-environment-files/blob/01d6e00c681c180f302885774add1537030ebb43/.config/nvim/lua/josean/plugins/formatting.lua>
 
 return {
-  "stevearc/conform.nvim",
-  event = {
-    -- バッファを読み込んだときに有効化
-    "BufReadPre",
-    "BufNewFile"
-  }, -- to disable, comment this out
-  opts = {},
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = {
+		-- バッファを読み込んだときに有効化
+		"BufReadPre",
+		"BufNewFile",
+	}, -- to disable, comment this out
+	opts = {},
+	config = function()
+		local conform = require("conform")
 
-    -- dprintが有効な場合のみdprintを返し、それ以外はformattersを返す
-    -- dprintが失敗すると`formatters_by_ft = { markdown = {{ "dprint", "prettier" }}`が正しく動作しない
-    local dprint_or_others = function(bufnr, formatters)
-      local dprint_available = require("conform").get_formatter_info("dprint", bufnr).available
-      -- `dprint.json`がneovimを開いているディレクトリに存在するか確認した結果をbooleanでflag変数に格納
-      local has_dprint_json = vim.fn.filereadable("dprint.json") == 1
-      if dprint_available and has_dprint_json then
-        return { "dprint" }
-      end
+		-- dprintが有効な場合のみdprintを返し、それ以外はformattersを返す
+		-- dprintが失敗すると`formatters_by_ft = { markdown = {{ "dprint", "prettier" }}`が正しく動作しない
+		local dprint_or_others = function(bufnr, formatters)
+			local dprint_available = require("conform").get_formatter_info("dprint", bufnr).available
+			-- `dprint.json`がneovimを開いているディレクトリに存在するか確認した結果をbooleanでflag変数に格納
+			local has_dprint_json = vim.fn.filereadable("dprint.json") == 1
+			if dprint_available and has_dprint_json then
+				return { "dprint" }
+			end
 
-      return formatters
-    end
+			return formatters
+		end
 
-    -- ファイルタイプごとのformatterの設定
-    -- 利用するformatterはmasonで管理
-    conform.setup({
-      formatters_by_ft = {
-        css = { "prettier" },
-        html = { "prettier" },
-        javascript = { "prettier" },
-        javascriptreact = { "prettier" },
-        json = function(bufnr) return dprint_or_others(bufnr, { "prettier" }) end,
-        lua = { "stylua" },
-        markdown = function(bufnr) return dprint_or_others(bufnr, { "prettier" }) end,
-        python = { "ruff" },
-        typescript = { "prettier" },
-        typescriptreact = { "prettier" },
-        yaml = { "prettier" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    })
+		-- ファイルタイプごとのformatterの設定
+		-- 利用するformatterはmasonで管理
+		conform.setup({
+			formatters_by_ft = {
+				css = { "prettier" },
+				html = { "prettier" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				json = function(bufnr)
+					return dprint_or_others(bufnr, { "prettier" })
+				end,
+				lua = { "stylua" },
+				markdown = function(bufnr)
+					return dprint_or_others(bufnr, { "prettier" })
+				end,
+				python = { "ruff" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				yaml = { "prettier" },
+			},
+			format_on_save = { lsp_fallback = true, async = false, timeout_ms = 1000 },
+		})
 
-    -- ショートカットキーの登録
-    vim.keymap.set(
-      { "n", "v" },
-      "<Plug>conform.format",
-      function()
-        conform.format({
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 1000,
-        })
-      end, { desc = "⭐︎Conform: Format file or range (in visual mode)" })
-  end,
+		local format_func = function()
+			conform.format({ lsp_fallback = true, async = false, timeout_ms = 1000 })
+		end
+		-- auto-saveプラグインから保存するときに自動でformatするための処理
+		local augroup = vim.api.nvim_create_augroup("comform-auto-save", { clear = true })
+		vim.api.nvim_create_autocmd(
+			{ "User" },
+			{ group = augroup, pattern = { "AutoSaveWritePre" }, callback = format_func }
+		)
+		-- ショートカットキーの登録
+		vim.keymap.set(
+			{ "n", "v" },
+			"<Plug>conform.format",
+			format_func,
+			{ desc = "⭐︎Conform: Format file or range (in visual mode)" }
+		)
+	end,
 }
