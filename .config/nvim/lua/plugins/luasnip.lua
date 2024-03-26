7e5ada441dc3ac16eb9d2a0a17240fc09b5f8b6b
iimuz <iz.primary.story+github@gmail.com>
2024-03-26 15:27:55 +0900

    luasnipの利用でneovimのプロジェクトルートにあるスニペットファイルを読むように修正

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -4,17 +4,19 @@
 -- Snippet管理プラグイン
 
 return {
-  "L3MON4D3/LuaSnip",
-  version = "v2.*",                -- follow latest release.
-  build = "make install_jsregexp", -- install jsregexp (optional!).
-  config = function()
-    require("luasnip.loaders.from_vscode").lazy_load(
-      {
-        paths = {
-          "./snippets",
-          "./snippets-private",
-        },
-      }
-    )
-  end,
+	"L3MON4D3/LuaSnip",
+	version = "v2.*", -- follow latest release.
+	build = "make install_jsregexp", -- install jsregexp (optional!).
+	config = function()
+		local cwd = vim.fn.getcwd()
+		local cwd_snippets_path = cwd .. "/.snippets"
+
+		require("luasnip.loaders.from_vscode").lazy_load({
+			paths = {
+				"./snippets",
+				"./snippets-private",
+				cwd_snippets_path,
+			},
+		})
+	end,
 }
