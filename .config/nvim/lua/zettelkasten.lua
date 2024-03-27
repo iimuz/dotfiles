20528bfcdeb4b7e21756325d7e61fcc9551130af
iimuz <iz.primary.story+github@gmail.com>
2024-03-27 10:16:15 +0900

    neovimのzettelkasten関連の設定がtelescopeの通常とかぶっていたので修正

--- .config/nvim/lua/zettelkasten.lua
+++ .config/nvim/lua/zettelkasten.lua
@@ -3,14 +3,8 @@
 -- 複数のプラグインに依存するため、プラグインの削除や追加に依存する
 
 local set = vim.keymap.set
-set(
-  "n",
-  "<Plug>(telescope.grep)",
-  function()
-    require("telescope.builtin").live_grep()
-    -- title検索をするための共通部分を挿入
-    vim.cmd("normal! ititle: .*")
-  end,
-  { desc = "⭐︎Zettelkasten: Search title in workspace." }
-)
-
+set("n", "<Plug>(zettelkasten.grep_title)", function()
+	require("telescope.builtin").live_grep()
+	-- title検索をするための共通部分を挿入
+	vim.cmd("normal! ititle: .*")
+end, { desc = "⭐︎Zettelkasten: Search title in workspace." })
