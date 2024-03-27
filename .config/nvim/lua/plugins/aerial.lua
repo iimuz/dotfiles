5510533dde1043c8e9971ccff7ae7b10a7904fc3
iimuz <iz.primary.story+github@gmail.com>
2024-03-27 10:16:56 +0900

    aerial.nvimのコマンド登録修正
    起動コマンドでnavしかなくwindowを起動したいことがあったので変更

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -7,78 +7,48 @@
 local condition = vim.g.vscode == nil
 
 -- 起動用のキーマッピング
-vim.keymap.set(
-  "n",
-  "<Plug>aerial.load",
-  "<cmd>AerialNavOpen<CR>",
-  { desc = "⭐︎Aerial: Load aerial(Outline plugin)." }
-)
+local set = vim.keymap.set
+set("n", "<Plug>(aerial.nav.open)", "<cmd>AerialNavOpen<CR>", { desc = "⭐︎Aerial: Open outline navigation." })
+set("n", "<Plug>(aerial.window.open)", "<cmd>AerialOpen!<CR>", { desc = "⭐︎Aerial: Open outline window." })
 
 return {
-  'stevearc/aerial.nvim',
-  cond = condition,
-  cmd = {
-    "AerialToggle",
-    "AerialOpen",
-    "AerialNavOpen",
-  },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons"
-  },
-  opts = {},
-  config = function()
-    local set = vim.keymap.set
-    require("aerial").setup({
-      -- Priority list of preferred backends for aerial.
-      -- This can be a filetype map (see :help aerial-filetype-map)
-      backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
-      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-      on_attach = function(bufnr)
-        -- Jump forwards/backwards with '{' and '}'
-        set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-        set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-      end,
-    })
-    -- Telescope用検索コマンド
-    set(
-      "n",
-      "<Plug>aerial.toggl",
-      "<cmd>AerialToggle!<CR>",
-      { desc = "Aerial: Toggle outline window." }
-    )
-    set(
-      "n",
-      "<Plug>aerial.open",
-      "<cmd>AerialOpen!<CR>",
-      { desc = "⭐︎Aerial: Open outline window." }
-    )
-    set(
-      "n",
-      "<Plug>aerial.close",
-      "<cmd>AerialClose<CR>",
-      { desc = "Aerial: Close outline window." }
-    )
-    set(
-      "n",
-      "<Plug>aerial.nav.open",
-      "<cmd>AerialNavOpen<CR>",
-      { desc = "⭐︎Aerial: Open outline navigation." }
-    )
-    set(
-      "n",
-      "<Plug>aerial.nav.close",
-      "<cmd>AerialNavClose<CR>",
-      { desc = "Aerial: Close outline navigation." }
-    )
+	"stevearc/aerial.nvim",
+	cond = condition,
+	cmd = {
+		"AerialToggle",
+		"AerialOpen",
+		"AerialNavOpen",
+	},
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {},
+	config = function()
+		local set = vim.keymap.set
+		require("aerial").setup({
+			-- Priority list of preferred backends for aerial.
+			-- This can be a filetype map (see :help aerial-filetype-map)
+			backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
+			-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+			on_attach = function(bufnr)
+				-- Jump forwards/backwards with '{' and '}'
+				set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+				set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+			end,
+		})
+		-- Telescope用検索コマンド
+		set("n", "<Plug>(aerial.toggl)", "<cmd>AerialToggle!<CR>", { desc = "Aerial: Toggle outline window." })
+		set("n", "<Plug>(aerial.close)", "<cmd>AerialClose<CR>", { desc = "Aerial: Close outline window." })
+		set("n", "<Plug>(aerial.nav.close)", "<cmd>AerialNavClose<CR>", { desc = "Aerial: Close outline navigation." })
 
-    -- Telescope拡張
-    require("telescope").load_extension("aerial")
-    set(
-      "n",
-      "<Plug>aerial.telescope",
-      "<cmd>Telescope aerial<CR>",
-      { desc = "Aerial: Show outline using Telescope" }
-    )
-  end,
+		-- Telescope拡張
+		require("telescope").load_extension("aerial")
+		set(
+			"n",
+			"<Plug>(aerial.telescope)",
+			"<cmd>Telescope aerial<CR>",
+			{ desc = "Aerial: Show outline using Telescope" }
+		)
+	end,
 }
