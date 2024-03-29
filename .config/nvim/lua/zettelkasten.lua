8f387a15b5f668aa85911e9b981803116f84c719
iimuz <iz.primary.story+github@gmail.com>
2024-03-29 16:27:13 +0900

    一時ファイルを開くコマンドを追加

--- .config/nvim/lua/zettelkasten.lua
+++ .config/nvim/lua/zettelkasten.lua
@@ -3,8 +3,16 @@
 -- 複数のプラグインに依存するため、プラグインの削除や追加に依存する
 
 local set = vim.keymap.set
+
+-- `title:`までを検索対象として追加したlive_grep
 set("n", "<Plug>(zettelkasten.grep_title)", function()
 	require("telescope.builtin").live_grep()
 	-- title検索をするための共通部分を挿入
 	vim.cmd("normal! ititle: .*")
 end, { desc = "⭐︎Zettelkasten: Search title in workspace." })
+
+-- 一時ファイルを作成して開く
+set("n", "<Plug>(zettelkasten.create_and_open_temp_file)", function()
+	local filepath = vim.fn.tempname()
+	vim.cmd("edit " .. filepath)
+end, { desc = "⭐︎Zettelkasten: Create and open temporary file." })
