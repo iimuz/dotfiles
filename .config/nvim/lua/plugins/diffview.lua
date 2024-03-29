7850171cb904933e193f6034c9ba627e85dd236d
iimuz <iz.primary.story+github@gmail.com>
2024-03-29 16:27:27 +0900

    特定ブランチとの差分を確認するコマンドを追加

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -7,37 +7,41 @@
 local condition = vim.g.vscode == nil
 
 return {
-  "sindrets/diffview.nvim",
-  cond = condition,
-  event = { "VimEnter" },
-  -- cmd = { "DiffviewOpen" },
-  config = function()
-    require("diffview").setup({})
+	"sindrets/diffview.nvim",
+	cond = condition,
+	event = { "VimEnter" },
+	-- cmd = { "DiffviewOpen" },
+	config = function()
+		require("diffview").setup({})
 
-    local set = vim.keymap.set
-    -- Telescope用検索コマンド
-    -- PR Review用に分岐元との差分を表示
-    -- see: <https://github.com/sindrets/diffview.nvim/blob/main/USAGE.md>
-    set(
-      "n",
-      "<Plug>(diffview.open_for_pr)",
-      -- diffviewopenを行うときにtab名を"hoge"に指定する
-      "<cmd>DiffviewOpen origin/HEAD...HEAD --imply-local --tabname=hoge<CR>",
-      { desc = "⭐︎Diffview: Open diff view for PR." }
-    )
-    set(
-      "n",
-      "<Plug>(diffview.open_for_large_pr)",
-      "<cmd>DiffviewFileHistory --range=origin/HEAD...HEAD --right-only --no-merges<CR>",
-      { desc = "Diffview: Open diff view for large PR." }
-    )
+		local set = vim.keymap.set
+		-- Telescope用検索コマンド
+		-- PR Review用に分岐元との差分を表示
+		-- see: <https://github.com/sindrets/diffview.nvim/blob/main/USAGE.md>
+		set(
+			"n",
+			"<Plug>(diffview.open_for_pr)",
+			"<cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<CR>",
+			{ desc = "⭐︎Diffview: Open diff view for PR." }
+		)
+		set(
+			"n",
+			"<Plug>(diffview.open_for_large_pr)",
+			"<cmd>DiffviewFileHistory --range=origin/HEAD...HEAD --right-only --no-merges<CR>",
+			{ desc = "Diffview: Open diff view for large PR." }
+		)
+		set("n", "<Plug>(diffview.open_for_pr_with_specific_branch)", function()
+			vim.ui.input({ prompt = "Enter base branch name: " }, function(branch_name)
+				local base_hash = vim.fn.system("git merge-base " .. branch_name .. " HEAD")
+				vim.cmd("DiffviewOpen " .. base_hash .. " ...HEAD --imply-local")
+			end)
+		end, { desc = "⭐︎Diffview: Open diff view for PR with spec." })
 
-    set(
-      "n",
-      "<Plug>(diffview.show_file_diff)",
-      "<cmd>DiffviewFileHistory %<CR>",
-      { desc = "Diffview: Open file history." }
-    )
-
-  end,
+		set(
+			"n",
+			"<Plug>(diffview.show_file_diff)",
+			"<cmd>DiffviewFileHistory %<CR>",
+			{ desc = "Diffview: Open file history." }
+		)
+	end,
 }
