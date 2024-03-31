9acb22a6d8d32940513eee9fb3c8a09a88b012b2
iimuz <iz.primary.story+github@gmail.com>
2024-03-31 22:59:45 +0900

    neovimのfoldをtreesitterで行うように修正

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,59 +5,60 @@
 local condition = vim.g.vscode == nil
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  cond = condition,
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	cond = condition,
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup(
-      {
-        ensure_installed = {
-          "bash",
-          "c",
-          "comment",
-          "cpp",
-          "css",
-          "csv",
-          "cuda",
-          "diff",
-          "dockerfile",
-          "git_config",
-          "git_rebase",
-          "gitattributes",
-          "gitcommit",
-          "gitignore",
-          "go",
-          "gomod",
-          "gosum",
-          "gowork",
-          "html",
-          "ini",
-          "javascript",
-          "jq",
-          "jsdoc",
-          "json",
-          "jsonc",
-          "lua",
-          "make",
-          "markdown",
-          "markdown_inline",
-          "mermaid",
-          "python",
-          "rust",
-          "toml",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "xml",
-          "yaml",
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      }
-    )
-  end,
+		configs.setup({
+			ensure_installed = {
+				"bash",
+				"c",
+				"comment",
+				"cpp",
+				"css",
+				"csv",
+				"cuda",
+				"diff",
+				"dockerfile",
+				"git_config",
+				"git_rebase",
+				"gitattributes",
+				"gitcommit",
+				"gitignore",
+				"go",
+				"gomod",
+				"gosum",
+				"gowork",
+				"html",
+				"ini",
+				"javascript",
+				"jq",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"lua",
+				"make",
+				"markdown",
+				"markdown_inline",
+				"mermaid",
+				"python",
+				"rust",
+				"toml",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"xml",
+				"yaml",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+		-- foldをtreesitterで行う
+		vim.opt.foldmethod = "expr"
+		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+		vim.opt.foldenable = false -- デフォルトでは折りたたみを無効にする
+	end,
 }
-
