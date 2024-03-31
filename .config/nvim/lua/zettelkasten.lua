1c751787a2c1375be6c9b75614d0bf328b67b701
iimuz <iz.primary.story+github@gmail.com>
2024-03-31 23:13:37 +0900

    front matterからtitle検索できるようにしたのでzettelkastenの簡易版を削除

--- .config/nvim/lua/zettelkasten.lua
+++ .config/nvim/lua/zettelkasten.lua
@@ -4,13 +4,6 @@
 
 local set = vim.keymap.set
 
--- `title:`までを検索対象として追加したlive_grep
-set("n", "<Plug>(zettelkasten.grep_title)", function()
-	require("telescope.builtin").live_grep()
-	-- title検索をするための共通部分を挿入
-	vim.cmd("normal! ititle: .*")
-end, { desc = "⭐︎Zettelkasten: Search title in workspace." })
-
 -- 一時ファイルを作成して開く
 set("n", "<Plug>(zettelkasten.create_and_open_temp_file)", function()
 	local filepath = vim.fn.tempname()
