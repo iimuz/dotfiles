fb4edd3a2926c2fdad11ed914d484c9ef43e2fbf
iimuz <iz.primary.story+github@gmail.com>
2024-04-19 15:54:33 +0900

    neovimにvscodeを呼び出す設定を追加

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,4 +6,5 @@ require("lazy-init") -- plugin managerとしてlazy.nvimを設定
 require("auto-lastmod") -- front matterのlatmod自動修正
 require("front-matter-searcher") -- front matterを検索するコマンド
 require("project-settings") -- project rootのneovim設定を読み込む
+require("vscode") -- vscodeの設定
 require("zettelkasten") -- zettelkasten関連の設定
