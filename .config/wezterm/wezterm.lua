7d74f620e595fbba6ab57d01340592623682cc8a
iimuz <iz.primary.story+github@gmail.com>
2024-04-22 08:09:54 +0900

    fix font size.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -15,13 +15,13 @@ config.color_scheme = "nord"
 config.window_background_opacity = 1.0
 -- font
 config.font = wezterm.font("Cica")
-config.font_size = 18.0
+config.font_size = 20.0
 -- 最大まで利用する
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 -- keybinds
