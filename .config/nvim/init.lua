87c78f0fe0b9ba8e0d101231b76e3797607056cb
iimuz <iz.primary.story+github@gmail.com>
2024-04-29 10:28:41 +0900

    :recycle: Builtin LSPのキーマップをwhich-keyに移動

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,5 @@
 require("base") -- プラグインなどに関係ない基本設定
 require("keymaps") -- プラグインに関係ないキーマップの基本設定
-require("lsp") -- Builtin LSP設定
 require("lazy-init") -- plugin managerとしてlazy.nvimを設定
 
 -- 以下は自作コマンドなどの設定
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -588,6 +588,53 @@ local function registerLspAndLlmKey()
 			},
 		},
 	}, { prefix = "<Leader>" })
+
+	-- LSP
+	-- LspAttachした
+	vim.api.nvim_create_autocmd("LspAttach", {
+		callback = function(ctx)
+			-- see: <https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#suggested-configuration>
+			require("which-key").register({
+				l = {
+					l = {
+						name = "LSP",
+						a = { vim.lsp.buf.code_action, "⭐︎LSP: Code action." },
+						d = { vim.lsp.buf.definition, "⭐︎LSP: Go to definition." },
+						D = { vim.lsp.buf.declaration, "⭐︎LSP: Go to declaration." },
+						e = {
+							name = "Diagnostic",
+							o = { vim.diagnostic.open_float, "⭐︎LSP: Show line diagnostics." },
+							p = { vim.diagnostic.goto_prev, "⭐︎LSP: Go to previous diagnostics." },
+							n = { vim.diagnostic.goto_next, "⭐︎LSP: Go to next diagnostics." },
+							l = { vim.diagnostic.setloclist, "⭐︎LSP: Set loclist diagnostics." },
+						},
+						f = {
+							function()
+								vim.lsp.buf.format({ async = true })
+							end,
+							"⭐︎LSP: Formatting.",
+						},
+						i = { vim.lsp.buf.implementation, "⭐︎LSP: Go to implementation." },
+						k = { vim.lsp.buf.signature_help, "⭐︎LSP: Show signature help." },
+						K = { vim.lsp.buf.hover, "⭐︎LSP: Show hover." },
+						n = { vim.lsp.buf.rename, "⭐︎LSP: Rename." },
+						r = { vim.lsp.buf.references, "⭐︎LSP: Show references." },
+						s = { "<cmd>LspInfo<CR>", "⭐︎LSP: Show lsp info." },
+						t = { vim.lsp.buf.type_definition, "⭐︎LSP: Type definition." },
+						w = {
+							name = "workspace",
+							a = { vim.lsp.buf.add_workspace_folder, "LSP: Add workspace folder." },
+							r = { vim.lsp.buf.remove_workspace_folder, "LSP: Remove workspace folder." },
+							l = { vim.lsp.buf.list_workspace_folders, "LSP: List workspace folders." },
+						},
+					},
+				},
+			}, {
+				prefix = "<leader>",
+				buffer = ctx.buff,
+			})
+		end,
+	})
 end
 
 -- Marks関連のキー登録
