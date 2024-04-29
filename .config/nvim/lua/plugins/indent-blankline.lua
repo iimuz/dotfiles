1f2e40fbd273c93f37604727cb7d0effd4939a5b
iimuz <iz.primary.story+github@gmail.com>
2024-04-29 10:06:43 +0900

    :sparkles: neovimにindentを可視化するプラグインを追加

---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,6 @@
+-- lukas-reineke/indent-blankline.nvim
+-- see: <https://github.com/lukas-reineke/indent-blankline.nvim>
+--
+-- インデントの可視化
+
+return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
