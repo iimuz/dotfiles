7849e379c98e77d9eeb9a5c05e41bad44e96eedd
iimuz <iz.primary.story+github@gmail.com>
2025-02-17 08:21:50 +0900

    :art: miseでgoを管理するように修正

--- .config/mise/mise-settings.sh
+++ .config/mise/mise-settings.sh
@@ -6,4 +6,9 @@
 if ! type mise > /dev/null 2>&1; then return 0; fi
 
 # eval "$($HOME/.local/bin/mise activate bash)"
-eval "$(mise activate bash)"
+if [ "$ZSH_VERSION" != "" ]; then  # zshの場合
+  eval "$(mise activate zsh)"
+else  # bashを想定
+  eval "$(mise activate bash)"
+fi
+
