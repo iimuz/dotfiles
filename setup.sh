a4a9443534be1b2af6362aba6be8baadcd3f5ccb
iimuz <iz.primary.story+github@gmail.com>
2025-08-31 10:21:06 +0900

    :art: claude codeとgemini cliの検出方法を修正

--- setup.sh
+++ setup.sh
@@ -79,14 +79,14 @@ if type bash >/dev/null 2>&1; then
 fi
 # === claude
 # claudeはaliasで登録されているため直接指定する必要がある
-if type $HOME/.claude/local/claude >/dev/null 2>&1; then
+if type claude >/dev/null 2>&1; then
   create_symlink $SCRIPT_DIR/.config/claude/agents $HOME/.claude/agents
   create_symlink $SCRIPT_DIR/.config/claude/commands $HOME/.claude/commands
   create_symlink $SCRIPT_DIR/.config/claude/settings.json $HOME/.claude/settings.json
   create_symlink $SCRIPT_DIR/.config/claude/CLAUDE.md $HOME/.claude/CLAUDE.md
 fi
 # === gemini cli
-if type $HOME/.claude/local/claude >/dev/null 2>&1; then
+if type gemini >/dev/null 2>&1; then
   create_symlink $SCRIPT_DIR/.config/gemini/settings.json $HOME/.gemini/settings.json
   create_symlink $SCRIPT_DIR/.config/gemini/GEMINI.md $HOME/.gemini/GEMINI.md
 fi
