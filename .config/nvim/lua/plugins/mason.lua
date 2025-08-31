71f90ee019e8a115bd2284eff01939c46e245aef
iimuz <iz.primary.story+github@gmail.com>
2025-08-31 10:54:47 +0900

    :art: mason toolをインストールする設定を追加

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -40,4 +40,26 @@ return {
 			})
 		end,
 	},
+	-- masonでツールを自動インストール
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		opts = {
+			ensure_installed = {
+				"cspell",
+				"cspell-lsp",
+				"shfmt",
+				"shellcheck",
+				"stylua",
+			},
+			auto_update = true,
+			run_on_start = true,
+			start_delay = 3000,
+			integrations = {
+				["mason-lspconfig"] = true,
+				["mason-null-ls"] = false,
+				["mason-nvim-dap"] = false,
+			},
+		},
+	},
 }
