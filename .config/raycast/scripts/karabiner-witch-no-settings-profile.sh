179d4df04fc7a2d9872f86425bb2ff93ef4d0320
iimuz <iz.primary.story+github@gmail.com>
2025-09-10 10:42:22 +0900

    :sparkles: raycastでkarabinerのprofileを変更するコマンドを追加

---
+++ .config/raycast/scripts/karabiner-witch-no-settings-profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Karabiner switch to no settings profile
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName System
+
+/Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --select-profile="No settings"
