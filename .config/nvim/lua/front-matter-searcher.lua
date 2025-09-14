bc2d6b02ed8794f5cd21ba719de9b13dac18671b
iimuz <iz.primary.story+github@gmail.com>
2025-09-14 20:04:56 +0900

    :art: front matter searcherをsnacks pickerに変更

--- .config/nvim/lua/front-matter-searcher.lua
+++ .config/nvim/lua/front-matter-searcher.lua
@@ -1,219 +1,96 @@
 -- front matterを利用した検索コマンド
 --
--- Telescopeに依存している。
+-- Snacks.nvimに依存している。
 
 local M = {}
 
--- VSCodeから利用する場合は無効化
-if vim.g.vscode ~= nil then
-	function M.setup() end
-	return M
-end
-
-local pickers = require("telescope.pickers") -- picker 作成用の API
-local finders = require("telescope.finders") -- finder 作成用の API
-local conf = require("telescope.config").values -- ユーザーの init.lua を反映した設定内容
-local entry_display = require("telescope.pickers.entry_display")
-
 -- rgでfront matterを取得して、yqでJSONに変換して返す
-local function getFrontMatter(select_query)
-	if select_query ~= "" then
-		select_query = select_query .. " |"
-	end
-
-	-- "---"で囲まれた部分をfront matterとして取得する
-	-- front matterはYAML形式で記述されている前提
-	-- front matterのため最初に見つかった部分のみを取得する
-	-- 下記の形式で取得される前提となっている。front matterの中は複数行に渡ることがある。
-	-- ```txt
-	-- path/to/file1.md
-	-- ---
-	-- title: hoge
-	-- description: fuga
-	-- ---
-	-- path/to/file2.md
-	-- ---
-	-- title: geho
-	-- description: gafu
-	-- ---
-	-- ```
-	local data = vim.fn.system(
-		"rg --multiline --multiline-dotall --max-count=1 --color=never --no-line-number --heading '^---\n(.*?)^---' '.'"
-	)
-
-	-- 全体がyaml形式になるように文字列を修正する
-	-- 下記のような形式になることを想定している
-	-- ```yaml
-	-- - path/to/file1.md
-	--   title: hoge
-	--   description: fuga
-	-- - path/to/file2.md
-	--   title: geho
-	--   description: gafu
-	-- ```
-	local data_lines = {}
-	for line in string.gmatch(data, "[^\r\n]+") do
-		if line ~= "---" then
-			if line:sub(1, 1) == "." then
-				data_lines[#data_lines + 1] = "- filepath: " .. line
-			else
-				data_lines[#data_lines + 1] = "  " .. line
-			end
-		end
-	end
-	local data_yml = table.concat(data_lines, "\n")
-
-	-- yqに渡すために一度、一時ファイルに書き出して実施
-	-- json linesで出力する
-	local tempname = vim.fn.tempname()
-	local tempfile = io.open(tempname, "w")
-	tempfile:write(data_yml)
-	tempfile:close()
-	local data_json = vim.fn.system(string.format("yq '.[] | %s @json' %s", select_query, tempname))
-
-	-- 1行ずつjsonを変換してテーブルに変換
-	local metadata_table = {}
-	for line in string.gmatch(data_json, "[^\r\n]+") do
-		metadata_table[#metadata_table + 1] = vim.json.decode(line)
-	end
-
-	return metadata_table
-end
-
--- rgでfront matterを取得して、yqでJSONに変換して返す
-local function getFrontMatter2(select_query)
-	if select_query ~= "" then
-		select_query = select_query .. " |"
-	end
-
-	-- "---"で囲まれた部分をfront matterとして取得する
-	-- front matterはYAML形式で記述されている前提
-	-- front matterのため最初に見つかった部分のみを取得する
-	-- 下記の形式で取得される前提となっている。front matterの中は複数行に渡ることがある。
-	-- ```txt
-	-- path/to/file1.md
-	-- ---
-	-- title: hoge
-	-- description: fuga
-	-- ---
-	-- path/to/file2.md
-	-- ---
-	-- title: geho
-	-- description: gafu
-	-- ---
-	-- ```
-	local data = vim.fn.system(
-		"rg --multiline --multiline-dotall --max-count=1 --color=never --no-line-number --heading '^---\n(.*?)^---' '.'"
-	)
-
-	-- 全体がyaml形式になるように文字列を修正する
-	-- 下記のような形式になることを想定している
-	-- ```yaml
-	-- - path/to/file1.md
-	--   title: hoge
-	--   description: fuga
-	-- - path/to/file2.md
-	--   title: geho
-	--   description: gafu
-	-- ```
-	local data_lines = {}
-	for line in string.gmatch(data, "[^\r\n]+") do
-		if line ~= "---" then
-			if line:sub(1, 1) == "." then
-				data_lines[#data_lines + 1] = "- filepath: " .. line
-			else
-				data_lines[#data_lines + 1] = "  " .. line
-			end
-		end
-	end
-	local data_yml = table.concat(data_lines, "\n")
-
-	-- yqに渡すために一度、一時ファイルに書き出して実施
-	-- json linesで出力する
-	local tempname = vim.fn.tempname()
-	local tempfile = io.open(tempname, "w")
-	tempfile:write(data_yml)
-	tempfile:close()
-	local data_json = vim.fn.system(string.format("yq '%s .[] | @json' %s", select_query, tempname))
-
-	-- 1行ずつjsonを変換してテーブルに変換
-	local metadata_table = {}
-	for line in string.gmatch(data_json, "[^\r\n]+") do
-		metadata_table[#metadata_table + 1] = vim.json.decode(line)
-	end
-
-	return metadata_table
-end
-
--- ファイルの情報を表示する
-local function makeDisplay(entry)
-	local metadata = entry.value
-	local displayer = entry_display.create({
-		separator = " ",
-		items = {
-			{ width = 40 },
-			{ remaining = true },
-		},
-	})
-
-	return displayer({
-		metadata.title,
-		{ metadata.filepath, "TelescopeResultsIdentifier" },
-	})
-end
-
--- front matterを取得してpickerを表示する
-function M.frontMatterPicker(opts)
-	opts = opts or {}
-	local select_query = opts.select_query or ""
-	pickers
-		.new(opts, {
-			prompt_title = "File",
-			finder = finders.new_table({
-				results = getFrontMatter(select_query),
-				entry_maker = function(entry)
-					return {
-						value = entry,
-						-- 検索対象として使われる文字列
-						ordinal = entry.title,
-						-- 画面上に表示される文字列
-						display = makeDisplay,
-						-- 選択したときに開くファイルのパス
-						path = entry.filepath,
-					}
-				end,
-			}),
-			sorter = conf.generic_sorter(opts),
-			previewer = conf.file_previewer(opts),
-		})
-		:find()
-end
 --
--- front matterを取得してpickerを表示する
-function M.frontMatterPicker2(opts)
-	opts = opts or {}
-	local select_query = opts.select_query or ""
-	pickers
-		.new(opts, {
-			prompt_title = "File",
-			finder = finders.new_table({
-				results = getFrontMatter2(select_query),
-				entry_maker = function(entry)
-					return {
-						value = entry,
-						-- 検索対象として使われる文字列
-						ordinal = entry.title,
-						-- 画面上に表示される文字列
-						display = makeDisplay,
-						-- 選択したときに開くファイルのパス
-						path = entry.filepath,
-					}
-				end,
-			}),
-			sorter = conf.generic_sorter(opts),
-			previewer = conf.file_previewer(opts),
-		})
-		:find()
+-- ## 処理の流れ
+--
+-- ### front matterの抽出
+-- rgで"---"で囲まれた部分をfront matterとして取得する
+-- front matterはYAML形式で記述されている前提
+-- front matterのため最初に見つかった部分のみを取得する
+-- 下記の形式で取得される前提となっている。front matterの中は複数行に渡ることがある。
+--
+-- ```txt
+-- path/to/file1.md
+-- ---
+-- title: hoge
+-- description: fuga
+-- ---
+-- path/to/file2.md
+-- ---
+-- title: geho
+-- description: gafu
+-- ---
+-- ```
+--
+-- ### pathを追加して複数ファイルのfront matterを結合したyamlへ変換
+--
+-- 次にjqで全体がyaml形式になるように文字列を修正する
+-- 下記のような形式になることを想定している
+--
+-- ```yaml
+-- - path/to/file1.md
+--   title: hoge
+--   description: fuga
+-- - path/to/file2.md
+--   title: geho
+--   description: gafu
+-- ```
+--
+-- ### yamlをjson linesへ変換
+--
+-- 最後にyqでyaml形式をパースして必要なデータを取得しjson形式で出力する
+-- 出力結果は、json lines形式となる。ただし、 `| @json` で出力しているので改行はない。
+-- `-o json` 形式の場合、整形して出力することになり `vim.json.decode` で読み込むことができない。
+--
+-- ```jsonl
+-- [
+--   {"path": "path1", "title": "title1"},
+--   {"path": "path2", "title": "title2"},
+-- ]
+-- ```
+local function getFrontMatter(select_query)
+	local command = [[
+rg --json --multiline --multiline-dotall --max-count=1 --color=never --no-line-number --heading '^---\n(.*?)^(---)' '.' |
+  jq -r 'select(.type == "match") | {path: .data.path.text, text: .data.lines.text | split("\n") | .[1:-2] | join("\n  ")} | "- path: \(.path)\n  \(.text)"' |
+  yq -p yaml]] .. " '" .. select_query .. " | @json'"
+	local data = vim.fn.system(command)
+	local metadata_table = vim.json.decode(data)
+
+	return metadata_table
+end
+
+-- snacks.nvimを利用したpicker設定
+function M.snacksPicker(opts)
+	local local_opts = opts or {}
+	local select_query = local_opts.select_query or "."
+
+	local finder = function()
+		local front_matter_table = getFrontMatter(select_query)
+
+		local items = {} ---@type snacks.picker.Item[]
+		for i, fm in ipairs(front_matter_table) do
+			---@type snacks.picker.Item
+			local item = {
+				idx = i,
+				score = 0,
+				text = fm.title,
+				file = fm.path,
+				filename = vim.fn.fnamemodify(fm.path, ":t"),
+			}
+			table.insert(items, item)
+		end
+		return items
+	end
+
+	require("snacks").picker({
+		finder = finder,
+		format = "text",
+	})
 end
 
 -- 基本となる処理の設定
