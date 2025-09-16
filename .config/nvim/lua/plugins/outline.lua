f3c2ca61ab382c929e2249e3cc079582a5147dbf
iimuz <iz.primary.story+github@gmail.com>
2025-09-16 09:42:00 +0900

    :bug: outline.nvimの初期化設定ミス

--- .config/nvim/lua/plugins/outline.lua
+++ .config/nvim/lua/plugins/outline.lua
@@ -7,11 +7,12 @@ return {
 	"hedyhli/outline.nvim",
 	lazy = true,
 	cmd = { "Outline", "OutlineOpen" },
+	opts = {},
 	keys = {
 		{ "<Leader>of", "<cmd>OutlineFollow<CR>", desc = "Outline: Go follow cursor position." },
 		{ "<Leader>oo", "<cmd>OutlineOpen<CR>", desc = "⭐︎Outline: Open." },
-		{ "<Leader>oq", "<cmd>OutlineClose<CR>", desc = "Outline: Close." },
 		{ "<Leader>os", "<cmd>OutlineStatus<CR>", desc = "Outline: Show status." },
 		{ "<Leader>or", "<cmd>OutlinesRefresh<CR>", desc = "Outline: Refresh of symbols." },
+		{ "<Leader>ox", "<cmd>OutlineClose<CR>", desc = "Outline: Close." },
 	},
 }
