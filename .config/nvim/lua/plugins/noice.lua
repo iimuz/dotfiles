77950429aac97d4aabb3283a8086cdda20a0b791
iimuz <iz.primary.story+github@gmail.com>
2025-09-20 18:37:14 +0900

    :art: neovimにnoice.nvimを追加

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+-- folke/noice.nvim
+-- see: <https://github.com/folke/noice.nvim>
+--
+-- UI
+
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+}
