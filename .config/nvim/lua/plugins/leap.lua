85cd439bbd406105fca9a240cb024610c8d46ce3
iimuz <iz.primary.story+github@gmail.com>
2026-01-30 09:50:49 +0900

    chore: :wrench: migrate leap.nvim plugin source to Codeberg

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -1,12 +1,12 @@
 -- leapの設定
--- see: <https://github.com/ggandor/leap.nvim>
+-- see: <https://codeberg.org/andyg/leap.nvim>
 --
 -- カーソル移動をラベルで行う。
 -- VSCodeから利用するときにeasymotionが利用できないので代わりに利用する。
 -- neovim単体であれば他のpluginも利用できるが操作性を変えたくないので同じpluginを利用する。
 
 return {
-	"ggandor/leap.nvim",
+	url = "https://codeberg.org/andyg/leap.nvim",
 	-- Do not set lazy loading via your fancy plugin manager
 	-- see: <https://github.com/ggandor/leap.nvim?tab=readme-ov-file#installation>
 	lazy = false,
