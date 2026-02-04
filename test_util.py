47d3249817dfafeb1538c2bdeedeb9bc88ae65d1
iimuz <iz.primary.story+github@gmail.com>
2026-02-04 16:57:26 +0900

    feat: :sparkles: add test utility module for demonstrations

---
+++ test_util.py
@@ -0,0 +1,9 @@
+"""Utility module for testing."""
+
+def calculate_sum(a, b):
+    """Calculate sum of two numbers."""
+    return a + b
+
+def get_user_name():
+    name = input("Enter name: ")
+    return name
