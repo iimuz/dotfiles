b95f3ba3b06140f52c2c2a578b9fd0ae59d90818
iimuz <iz.primary.story+github@gmail.com>
2026-07-30 11:24:53 +0900

    chore: update claude setting

--- setup_mac.sh
+++ setup_mac.sh
@@ -72,7 +72,9 @@ if type claude >/dev/null 2>&1; then
   create_symlink "$SCRIPT_DIR/.config/claude/marketplace" "$HOME/.claude/marketplace"
   claude plugin marketplace add "$HOME/.claude/marketplace"
   claude plugin install "six-hats@iimuz-dotfiles"
+  claude plugin disable "six-hats@iimuz-dotfiles"
   claude plugin install "scamper@iimuz-dotfiles"
+  claude plugin disable "scamper@iimuz-dotfiles"
 fi
 # === gh
 if type gh >/dev/null 2>&1; then
