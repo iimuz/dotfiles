199d1784209731f6177bbea5002f07f80c9e8ad0
iimuz <iz.primary.story+github@gmail.com>
2026-08-28 17:09:15 +0900

    feat: AFK 用と復帰用の set-mac スクリプトを追加
    - set-mac-afk.sh: 画面を真っ暗にして、音量をミュートし、マイクをミュート
    - set-mac-default.sh: 画面の輝度を 0.5 に戻し、音量を 30 に戻し、マイクをアンミュート

---
+++ .config/raycast/scripts/set-mac-default.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Set mac settings for default.
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName System
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" >/dev/null 2>&1 && pwd)"
+
+"$SCRIPT_DIR/lib/set-brightness.sh" 0.5
+"$SCRIPT_DIR/lib/set-volume.sh" 30
+"$SCRIPT_DIR/toggle-microphone.sh" unmute >/dev/null
